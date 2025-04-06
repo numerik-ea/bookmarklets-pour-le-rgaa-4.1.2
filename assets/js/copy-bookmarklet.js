b720eb9c962725dcf57d7e631a0ba1d46b15d02c
Florent Roques <florentroques@users.noreply.github.com>
2025-04-06 23:56:23 +0200

    ADD tooltip when clicking on the copy bookmarklet button

--- assets/js/copy-bookmarklet.js
+++ assets/js/copy-bookmarklet.js
@@ -2,6 +2,14 @@ function copyBookmarklet(button) {
   const bookmarkletLink = button.previousElementSibling;
   const bookmarkletContent = bookmarkletLink.getAttribute("href");
 
+  // Create tooltip if it doesn't exist
+  if (!button.querySelector('.tooltip')) {
+    const tooltip = document.createElement('span');
+    tooltip.className = 'tooltip';
+    tooltip.textContent = 'Bookmarklet copié dans le presse-papiers';
+    button.appendChild(tooltip);
+  }
+
   navigator.clipboard
     .writeText(bookmarkletContent.replace("%C2%A0", "&nbsp;"))
     .then(() => {
@@ -11,6 +19,7 @@ function copyBookmarklet(button) {
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <polyline points="20 6 9 17 4 12"></polyline>
             </svg>
+            <span class="tooltip show">Bookmarklet copié dans le presse-papiers</span>
         `;
 
       setTimeout(() => {
