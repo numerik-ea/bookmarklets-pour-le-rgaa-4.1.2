782af13816413fd82e0099b227b03a6f080e533f
Florent Roques <florentroques@users.noreply.github.com>
2025-04-07 09:22:31 +0200

    Hide copy icon from AT + add tooltip on hover of copy bookmarklet button

--- assets/js/copy-bookmarklet.js
+++ assets/js/copy-bookmarklet.js
@@ -16,7 +16,7 @@ function copyBookmarklet(button) {
       // Visual feedback
       const originalHTML = button.innerHTML;
       button.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <polyline points="20 6 9 17 4 12"></polyline>
             </svg>
             <span class="tooltip show">Bookmarklet copié dans le presse-papiers</span>
