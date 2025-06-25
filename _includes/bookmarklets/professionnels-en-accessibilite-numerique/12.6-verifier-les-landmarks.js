765b6851710b94f8e13ce69da5ddaddaff1f79b3
Florent Roques <florentroques@users.noreply.github.com>
2025-06-25 15:55:33 +0200

    Better formatting with double quotes in messages

--- _includes/bookmarklets/professionnels-en-accessibilite-numerique/12.6-verifier-les-landmarks.js
+++ _includes/bookmarklets/professionnels-en-accessibilite-numerique/12.6-verifier-les-landmarks.js
@@ -12,7 +12,7 @@
         const tagName = mapRoleThatMustBeUniqueToTagName[role];
 
         if (elements.length === 0) {
-            message += `Aucun élément avec role='${role}'.\n`;
+            message += `Aucun élément avec role="${role}".\n`;
 
             elements = document.querySelectorAll(tagName);
 
@@ -20,22 +20,22 @@
                 message += `Aucun élément <${tagName}> trouvé.\n`;
             } else if (elements.length > 1) {
                 message += `Plusieurs éléments <${tagName}> trouvés.\n`;
-                message += `Identifier le bon élément et ajouter l'attribut role='${role}' à celui-ci.\n`;
+                message += `Identifier le bon élément et ajouter l'attribut role="${role}" à celui-ci.\n`;
             } else {
                 message += `Un élément <${tagName}> trouvé.\n`;
-                message += `Vérifier que c'est bien le bon élément <${tagName}> et lui ajouter l'attribut role='${role}'.\n`;
+                message += `Vérifier que c'est bien le bon élément <${tagName}> et lui ajouter l'attribut role="${role}".\n`;
             }
         } else if (elements.length > 1) {
             message += `12.6 NC : [role='${role}'] trouvé mais plusieurs éléments avec ce rôle :\n`;
 
             elements.forEach(element => {
-                message += `<${element.tagName.toLowerCase()} role='${role}'>\n`;
+                message += `<${element.tagName.toLowerCase()} role="${role}">\n`;
             });
 
             message += `Le rôle WAI-ARIA ${role} doit être unique sur la page\n`;
         } else {
-            message += `Un élément avec role='${role}' trouvé :`;
-            message += `<${elements[0].tagName.toLowerCase()} role='${role}'>\n`;
+            message += `Un élément avec role="${role}" trouvé :`;
+            message += `<${elements[0].tagName.toLowerCase()} role="${role}">\n`;
         }
     }
 
@@ -54,22 +54,22 @@
             message += `Aucun élément <${tagName}> trouvé.\n`;
         } else if (navElements.length > 1) {
             message += `Plusieurs éléments <${tagName}> trouvés.\n`;
-            message += `Identifier les bons éléments <${tagName}> et ajouter l'attribut role='navigation' à ceux-ci.\n`;
+            message += `Identifier les bons éléments <${tagName}> et ajouter l'attribut role="navigation" à ceux-ci.\n`;
         } else {
             message += `Un élément <${tagName}> trouvé.\n`;
-            message += `Vérifier que c'est bien le bon élément et lui ajouter l'attribut role='navigation'.\n`;
+            message += `Vérifier que c'est bien le bon élément et lui ajouter l'attribut role="navigation".\n`;
         }
     } else if (roleNavigationElements.length > 1) {
-        message += `Plusieurs éléments avec role='navigation' trouvés.\n`;
+        message += `Plusieurs éléments avec role="navigation" trouvés.\n`;
 
         roleNavigationElements.forEach(element => {
-            message += `<${element.tagName.toLowerCase()} role='navigation'>\n`;
+            message += `<${element.tagName.toLowerCase()} role="navigation">\n`;
         });
 
-        message += `Identifier le bon élément et ajouter l'attribut role='navigation' à celui-ci.\n`;
+        message += `Identifier le bon élément et ajouter l'attribut role="navigation" à celui-ci.\n`;
     } else {
-        message += `Un élément avec role='navigation' trouvé :`;
-        message += `<${roleNavigationElements[0].tagName.toLowerCase()} role='navigation'>\n`;
+        message += `Un élément avec role="navigation" trouvé :`;
+        message += `<${roleNavigationElements[0].tagName.toLowerCase()} role="navigation">\n`;
     }
 
     message += '\n';
@@ -77,19 +77,19 @@
     const roleSearchElements = document.querySelectorAll(`[role='search']`);
 
     if (roleSearchElements.length === 0) {
-        message += `Aucun élément avec role='search'.\n`;
+        message += `Aucun élément avec role="search".\n`;
     } else if (roleSearchElements.length > 1) {
-        message += `Plusieurs éléments avec role='search' trouvés.\n`;
+        message += `Plusieurs éléments avec role="search" trouvés.\n`;
 
         roleSearchElements.forEach(element => {
-            message += `<${element.tagName.toLowerCase()} role='search'>\n`;
+            message += `<${element.tagName.toLowerCase()} role="search">\n`;
         });  
 
-        message += `Identifier le bon élément et ajouter l'attribut role='search' à celui-ci.\n`;
+        message += `Identifier le bon élément et ajouter l'attribut role="search" à celui-ci.\n`;
         message += `Le moteur de recherche doit permettre d'effectuer des recherches sur les contenus de l'ensemble du site.\n`;
     } else {
-        message += `Un élément avec role='search' trouvé :`;
-        message += `<${roleSearchElements[0].tagName.toLowerCase()} role='search'>\n`;
+        message += `Un élément avec role="search" trouvé :`;
+        message += `<${roleSearchElements[0].tagName.toLowerCase()} role="search">\n`;
     }
 
     message += '\n';
