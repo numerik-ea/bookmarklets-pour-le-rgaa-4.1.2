8ebedbbb60c9220fa44ecb461d4199dc6eff36af
Florent Roques <florentroques@users.noreply.github.com>
2025-10-28 18:16:25 +0100

    10.12 | 12.8 plus un 10.1 CSS seulement

---
+++ _includes/bookmarklets/professionnels-en-accessibilite-numerique/10.12-espacement-des-caracteres-[TOGGLE].js
@@ -0,0 +1,25 @@
+(function () {
+  var id = "a11y-text-spacing-style";
+  var el = document.getElementById(id);
+
+  if (el) {
+    el.remove();
+    alert("Espacement des caractères désactivé");
+  } else {
+    var css = `html body * {
+            line-height: 1.5 !important;
+            letter-spacing: 0.12em !important;
+            word-spacing: 0.16em !important;
+        }
+        
+        html body p {
+            margin-bottom: 2em !important;
+        }`;
+
+    var s = document.createElement("style");
+    s.id = id;
+    s.appendChild(document.createTextNode(css));
+    document.head.appendChild(s);
+    alert("Espacement des caractères activé");
+  }
+})();
