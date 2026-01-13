bc182bb725538792fe3d5280a4425e3c8baeed55
Florent Roques <florentroques@users.noreply.github.com>
2026-01-13 14:41:26 +0100

    Support shadow DOM in deprecated tags highlighter
    Enhanced the bookmarklet to inject and remove highlighting styles within all shadow roots in addition to the main document. Also expanded the list of deprecated attributes detected and highlighted, improving coverage for accessibility testing.

--- _includes/bookmarklets/professionnels-en-accessibilite-numerique/10.1-test-presence-balises-et-attributs-de-presentation-seulement-CSS-TOGGLE.js
+++ _includes/bookmarklets/professionnels-en-accessibilite-numerique/10.1-test-presence-balises-et-attributs-de-presentation-seulement-CSS-TOGGLE.js
@@ -1,10 +1,67 @@
 // prettier-ignore
 (function () {
+  // Function to recursively get all shadow roots
+  function getAllShadowRoots(root = document) {
+    const shadowRoots = [];
+    const walker = document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT);
+    let node;
+    while ((node = walker.nextNode())) {
+      if (node.shadowRoot) {
+        shadowRoots.push(node.shadowRoot);
+        // Recursively get shadow roots within shadow roots
+        shadowRoots.push(...getAllShadowRoots(node.shadowRoot));
+      }
+    }
+    return shadowRoots;
+  }
+
+  // Function to find style element by ID in a root
+  function findStyleElement(root, id) {
+    if (root === document) {
+      return document.getElementById(id);
+    } else {
+      // For shadow roots, use querySelector
+      return root.querySelector('style#' + id);
+    }
+  }
+
+  // Function to inject styles into a root
+  function injectStyles(root, cssText, id) {
+    const style = document.createElement('style');
+    style.id = id;
+    style.appendChild(document.createTextNode(cssText));
+    if (root === document) {
+      root.head.appendChild(style);
+    } else {
+      // For shadow roots, append to the shadow root itself
+      root.appendChild(style);
+    }
+  }
+
+  // Function to remove styles from a root
+  function removeStyles(root, id) {
+    const el = findStyleElement(root, id);
+    if (el) {
+      el.remove();
+      return true;
+    }
+    return false;
+  }
+
   var id = 'a11y-deprecated-presentation-tags-and-attributes-highlighter';
   var el = document.getElementById(id);
+  var shadowRoots = getAllShadowRoots();
+  var hasShadowStyles = shadowRoots.some(root => findStyleElement(root, id));
 
-  if (el) {
-    el.remove();
+  if (el || hasShadowStyles) {
+    // Remove from document
+    if (el) {
+      el.remove();
+    }
+    // Remove from all shadow roots (check again in case new ones were created)
+    getAllShadowRoots().forEach(root => {
+      removeStyles(root, id);
+    });
     alert('Mise en évidence des balises et attributs dépréciés : DÉSACTIVÉE');
   } else {
     var css = `/** disclaimer : si plusieurs attributs sur même élément : seul le dernier déclaré est révélé **/
@@ -33,15 +90,26 @@
     [basefont],
     [bgcolor],
     [border],
-    [color],
-    [link],
-    [text],
-    [vlink],
     [cellpadding],
     [cellspacing],
+    [char],
+    [charoff],
+    [clear],
+    [compact],
+    [color],
+    [frameborder],
+    [hspace],
+    [link],
+    [marginheight],
+    [marginwidth],
+    [text],
+    [valign],
+    [vlink],
+    [vspace],
     :not(img,svg,canvas,embed,object,rect,source)[width],
     :not(img,svg,canvas,embed,object,rect,source)[height],
-    :not(img,svg,canvas,embed,object,rect,source)[width][height] {
+    :not(img,svg,canvas,embed,object,rect,source)[width][height],
+    :not(select)[size] {
         border: 3px solid var(--border-color);
         outline: 3px solid var(--border-color);
     }
@@ -156,6 +224,56 @@
         background-color: var(--msg-bgcolor);
     }
     
+    [char]::before {
+        content: '[attr:char]';
+        background-color: var(--msg-bgcolor);
+    }
+    
+    [charoff]::before {
+        content: '[attr:charoff]';
+        background-color: var(--msg-bgcolor);
+    }
+    
+    [clear]::before {
+        content: '[attr:clear]';
+        background-color: var(--msg-bgcolor);
+    }
+    
+    [compact]::before {
+        content: '[attr:compact]';
+        background-color: var(--msg-bgcolor);
+    }
+    
+    [frameborder]::before {
+        content: '[attr:frameborder]';
+        background-color: var(--msg-bgcolor);
+    }
+    
+    [hspace]::before {
+        content: '[attr:hspace]';
+        background-color: var(--msg-bgcolor);
+    }
+    
+    [marginheight]::before {
+        content: '[attr:marginheight]';
+        background-color: var(--msg-bgcolor);
+    }
+    
+    [marginwidth]::before {
+        content: '[attr:marginwidth]';
+        background-color: var(--msg-bgcolor);
+    }
+    
+    [valign]::before {
+        content: '[attr:valign]';
+        background-color: var(--msg-bgcolor);
+    }
+    
+    [vspace]::before {
+        content: '[attr:vspace]';
+        background-color: var(--msg-bgcolor);
+    }
+    
     :not(img,svg,canvas,embed,object,rect,source)[width]::before {
         content: '[attr:width]';
         background-color: var(--msg-bgcolor);
@@ -169,12 +287,21 @@
     :not(img,svg,canvas,embed,object,rect,source)[width][height]::before {
         content: '[attr:height & width]';
         background-color: var(--msg-bgcolor);
+    }
+    
+    :not(select)[size]::before {
+        content: '[attr:size]';
+        background-color: var(--msg-bgcolor);
     }`;
 
-    var s = document.createElement('style');
-    s.id = id;
-    s.appendChild(document.createTextNode(css));
-    document.head.appendChild(s);
+    // Inject styles into document
+    injectStyles(document, css, id);
+    
+    // Inject styles into all shadow roots (check again in case new ones were created)
+    getAllShadowRoots().forEach(root => {
+      injectStyles(root, css, id);
+    });
+    
     alert('Mise en évidence des balises et attributs dépréciés : ACTIVÉE');
   }
 })();
